39920c372a856f0206632ae415716bacd5b76b95
Migue <migue@pop-os.localdomain>
2022-12-11 18:17:28 -0300

    feat: add basic scene

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,36 @@
 <template>
   <div>
-    <h1>hola mundo</h1>
+    el path: {{ JSON.stringify(currentPath) }}
+    <component :is="currentView" />
   </div>
 </template>
 
 <script>
-export default {};
+import Admin from "./pages/Admin.vue";
+import Scene from "./pages/Scene.vue";
+
+const routes = {
+  "/": Admin,
+  scene: Scene,
+};
+
+export default {
+  data() {
+    return {
+      currentPath: window.location.pathname,
+    };
+  },
+  computed: {
+    currentView() {
+      return routes[this.currentPath.slice(1) || "/"] || Admin;
+    },
+  },
+  mounted() {
+    window.addEventListener("hashchange", () => {
+      this.currentPath = window.location.pathname;
+    });
+  },
+};
 </script>
 
 <style>
